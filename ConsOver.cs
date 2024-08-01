56c2fb84f66f613ed7da3748c299bef3d1345922
Shaikhhamza29 <Shaikhhamza29@gmail.com>
2024-08-01 22:54:08 +0530

    Constructor Overloading

--- ConsOver.cs
+++ ConsOver.cs
@@ -1,23 +1,26 @@
-class ConsOver
+class ConsOver1
 {
 
-	public ConsOver(int a , int b)
+	public ConsOver1(int a , int b)
 	{
 		int result = a * b;
 		System.Console.WriteLine("the Multiplication of {0} * {1} = {2}",a,b,result);
 	}
 
-	public ConsOver(double c , double d)
+	public ConsOver1(double c , double d)
 	{
 		double result = c * d ;
 		System.Console.WriteLine("the Mutiplcation of "+c+" * "+d+ " = "+result);
 	}
+}
 
+class ConsOver
+{
 	public static void Main(string[] args)
 	{
 
-		ConsOver obj1 = new ConsOver(5,8);
-		ConsOver obj2 = new ConsOver(5.9,8.6);
+		ConsOver1 obj1 = new ConsOver1(5,8);
+		ConsOver1 obj2 = new ConsOver1(5.9D,8.6D);
 
 	}	
 }
